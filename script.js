501ef95a5db9fa56f46df55d0cb031f7cb5e86f2
iSajmon <sz.caldera@gmail.com>
2024-03-13 20:55:02 +0100

    UPDATE

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function showPage(url) {
  }, 3000);  
 }
 
+function changeStyle() {
+  const style = qs('#style');
+  const styleMobile = qs('#styleMobile');
+ 
+  if(style.href.includes('style.css')) {
+    style.href = 'styleDark.css'
+    styleMobile.href = 'styleMobileDark.css'
+  }else {
+    style.href = 'style.css'
+    styleMobile.href = 'styleMobile.css'
+  }
+    
+  
+}
